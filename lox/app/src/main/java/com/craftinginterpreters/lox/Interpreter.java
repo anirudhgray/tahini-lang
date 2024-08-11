8c083d5db83a2c854ca41180ef490b937e7c8e1a
anirudh <anirudh04mishra@gmail.com>
2024-08-11 14:29:25 +0530

    feat: statements and state
    - add support for statements print and var
    - add global var support in environment

--- lox/app/src/main/java/com/craftinginterpreters/lox/Interpreter.java
+++ lox/app/src/main/java/com/craftinginterpreters/lox/Interpreter.java
@@ -1,11 +1,16 @@
 package com.craftinginterpreters.lox;
 
-class Interpreter implements Expr.Visitor<Object> {
+import java.util.List;
 
-    void interpret(Expr expression) {
+class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+
+    private Environment environment = new Environment();
+
+    void interpret(List<Stmt> statements) {
         try {
-            Object value = evaluate(expression);
-            System.out.println(stringify(value));
+            for (Stmt statement : statements) {
+                execute(statement);
+            }
         } catch (RuntimeError error) {
             Lox.runtimeError(error);
         }
@@ -41,6 +46,30 @@ class Interpreter implements Expr.Visitor<Object> {
         return expr.accept(this);
     }
 
+    private void execute(Stmt stmt) {
+        stmt.accept(this);
+    }
+
+    @Override
+    public Void visitExpressionStmt(Stmt.Expression stmt) {
+        evaluate(stmt.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitPrintStmt(Stmt.Print stmt) {
+        Object value = evaluate(stmt.expression);
+        System.out.println(stringify(value));
+        return null;
+    }
+
+    @Override
+    public Void visitVarStmt(Stmt.Var stmt) {
+        Object initVal = evaluate(stmt.initializer);
+        environment.define(stmt.name.lexeme, initVal);
+        return null;
+    }
+
     @Override
     public Object visitUnaryExpr(Expr.Unary expr) {
         Object right = evaluate(expr.right);
@@ -55,6 +84,11 @@ class Interpreter implements Expr.Visitor<Object> {
             default ->
                 null;
         };
+    }
+
+    @Override
+    public Object visitVariableExpr(Expr.Variable expr) {
+        return environment.getValue(expr.name);
     }
 
     private void checkNumberOperand(Token operator, Object operand) {
@@ -158,5 +192,13 @@ class Interpreter implements Expr.Visitor<Object> {
         Object left = evaluate(expr.left);
         Object right = evaluate(expr.right);
         return isTruthy(condition) ? left : right;
+    }
+
+    @Override
+    public Object visitAssignExpr(Expr.Assign expr) {
+        Token variable = expr.name;
+        Object value = evaluate(expr.value);
+        environment.assign(variable, value);
+        return value;
     }
 }
