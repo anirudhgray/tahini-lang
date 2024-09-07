3c5780f43b4bcc97e4be698f50ce4de477420238
anirudh <anirudh04mishra@gmail.com>
2024-09-07 21:44:00 +0530

    feat: function callables

--- lox/app/src/main/java/com/craftinginterpreters/lox/Interpreter.java
+++ lox/app/src/main/java/com/craftinginterpreters/lox/Interpreter.java
@@ -1,5 +1,6 @@
 package com.craftinginterpreters.lox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
@@ -9,11 +10,28 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
     final boolean repl;
 
+    final Environment globals = new Environment();
+    private Environment environment = globals;
+
     public Interpreter(boolean repl) {
         this.repl = repl;
-    }
+        globals.define("clock", new LoxCallable() {
+            @Override
+            public int arity() {
+                return 0;
+            }
 
-    private Environment environment = new Environment();
+            @Override
+            public Object call(Interpreter interpreter, List<Object> arguments) {
+                return (double) System.currentTimeMillis() / 1000.0;
+            }
+
+            @Override
+            public String toString() {
+                return "<native fn>";
+            }
+        });
+    }
 
     void interpret(List<Stmt> statements) {
         try {
@@ -226,6 +244,30 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
                 null;
         };
 
+    }
+
+    @Override
+    public Object visitCallExpr(Expr.Call expr) {
+        Object callee = evaluate(expr.callee);
+
+        List<Object> arguments = new ArrayList<>();
+        for (Expr argument : expr.arguments) {
+            arguments.add(evaluate(argument));
+        }
+
+        if (!(callee instanceof LoxCallable)) {
+            throw new RuntimeError(expr.paren,
+                    "Can only call functions and classes.");
+        }
+
+        LoxCallable function = (LoxCallable) callee;
+        if (arguments.size() != function.arity()) {
+            throw new RuntimeError(expr.paren, "Expected "
+                    + function.arity() + " arguments but got "
+                    + arguments.size() + ".");
+        }
+
+        return function.call(this, arguments);
     }
 
     private void checkNumberOperands(Token operator, Object a, Object b) {
